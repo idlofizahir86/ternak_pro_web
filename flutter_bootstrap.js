21a11a3bbdac558440f82bc583a9c478a53303c1
idlofizahir86 <idlofizahir86@gmail.com>
2025-09-13 08:21:48 +0700

    App Hampir Final

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3660184747"
+    serviceWorkerVersion: "2920214341"
   }
 });
